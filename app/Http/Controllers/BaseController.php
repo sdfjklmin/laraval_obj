85f0816b7091df346a8a7dd1f9f8a8a0b2257c7b
sdfjklmin <735305610@qq.com>
2017-03-27 16:46:44 +0800

    min

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -19,6 +19,6 @@ class BaseController extends Controller
   
     public function index()
     {
-        return view('base')->withMenu(\App\Menu::all());
+        return view('base');
     }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-         return view('home')->withArticles(\App\Article::all());
+         return view('home');
     }
 }
