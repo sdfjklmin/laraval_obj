fe690e88fbd2340098e81e6f6e297e60d81548e1
sdfjklmin <735305610@qq.com>
2017-03-27 15:56:01 +0800

    min

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        echo 'base controller' ;exit();
+         return view('home')->withArticles(\App\Article::all());
+    }
+}
