ebe91a925e76a8b109ee1fc6ffb5bc3c44d5d8f1
sdfjklmin <735305610@qq.com>
2017-03-28 17:31:05 +0800

    min

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -2,18 +2,5 @@
 
 @section('content')
 hahahha ;
-@foreach ($menus as $menu)
-                        <hr>
-                        <div class="article">
-                            <h4>{{ $menu->name }}</h4>
-                            <div class="content">
-                            </div>
-                        </div>
-                        <a href="{{ url('admin/article/'.$article->id.'/edit') }}" class="btn btn-success">编辑</a>
-                        <form action="{{ url('admin/article/'.$article->id) }}" method="POST" style="display: inline;">
-                            {{ method_field('DELETE') }}
-                            {{ csrf_field() }}
-                            <button type="submit" class="btn btn-danger">删除</button>
-                        </form>
-                    @endforeach
+
 @endsection
